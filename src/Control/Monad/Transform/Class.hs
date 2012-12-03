e50293bf42e405065198ec88ea86d7ac01dff857
Tony Morris <tmorris@tmorris.net>
2012-12-03 17:41:20 +1000

    bind transform

--- src/Control/Monad/Transform/Class.hs
+++ src/Control/Monad/Transform/Class.hs
@@ -1,7 +1,11 @@
 {-# LANGUAGE Rank2Types #-}
 
-module Control.Monad.Transform.Class(MonadTransform(..)) where
+module Control.Monad.Transform.Class(
+  BindTransform(..)
+, MonadTransform(..)
+) where
 
+import Data.Functor.Bind
 import Data.Functor.Identity
 import Control.Monad.Trans.Identity
 import Control.Monad.Trans.Reader
@@ -12,7 +16,46 @@ import Control.Monad.Trans.Maybe
 import Control.Monad.Trans.List
 import Control.Monad.Trans.Error
 
-class MonadTransform t where
+class BindTransform t where
+  transformB ::
+    (Bind f, Bind g) =>
+    (forall z. f z -> g z)
+    -> t f a
+    -> t g a
+
+instance BindTransform IdentityT where
+  transformB f (IdentityT k) =
+    IdentityT (f k)
+
+instance BindTransform (ReaderT r) where
+  transformB f (ReaderT k) =
+    ReaderT (f . k)
+
+instance BindTransform (WriterT w) where
+  transformB f (WriterT k) =
+    WriterT (f k)
+
+instance BindTransform (StateT s) where
+  transformB f (StateT k) =
+    StateT (f . k)
+
+instance BindTransform (RWST r w s) where
+  transformB f (RWST k) =
+    RWST (\r -> f . k r)
+
+instance BindTransform MaybeT where
+  transformB f (MaybeT k) =
+    MaybeT (f k)
+
+instance BindTransform ListT where
+  transformB f (ListT k) =
+    ListT (f k)
+
+instance BindTransform (ErrorT e) where
+  transformB f (ErrorT k) =
+    ErrorT (f k)
+
+class BindTransform t => MonadTransform t where
   transform ::
     (Monad f, Monad g) =>
     (forall z. f z -> g z)
